3e619963671828166bf6aaf8311384c08ce6b897
CatsInPartyPants <81612454+CatsInPartyPants@users.noreply.github.com>
2022-07-08 22:15:36 +0300

    3 done

--- topHW/homework10-2.cpp
+++ topHW/homework10-2.cpp
@@ -9,18 +9,40 @@ void showarr(int arr[][3], int size);
 //task 2
 void makingArr2(int arr[][3], int size, int userElement);
 
+//task 3
+void moveArray(int arr[2][10], int howMuchMove, char whichWay);
 
 int main()
 {
+    
     int userElement;
     cout << "Enter first element of the array->";
     cin >> userElement;
     int myArray[3][3];
     int myArray2[3][3];
+    //1
     makingArr(myArray,3, userElement);
     showarr(myArray,3);
+    //2
     makingArr2(myArray2, 3, userElement);
     showarr(myArray2, 3);
+    //3 
+    int arrayForConvert[2][10] ={
+                                {1,2,3,4,5,6,7,8,9,10},
+                                {9,8,7,6,5,4,3,2,1,0}
+                                };
+    
+    moveArray(arrayForConvert, 3, 'r');
+    moveArray(arrayForConvert, 3, 'l');
+   //show array
+   for(int i = 0; i<2; i++)
+   {
+       for(int j = 0; j<10; j++)
+       {
+           cout << arrayForConvert[i][j] << " ";
+       }
+       cout << endl;
+   }
     return 0;
 }
 
@@ -66,11 +88,48 @@ void makingArr2(int arr[][3], int size, int userElement)
         if(i == 0 && j == 0)
         {
             arr[0][0] = temp;
-            temp ++;
+            temp++;
         }
         else{
             arr[i][j] = temp;
             temp++;
+        }
+    }
+}
+
+//task 3 Function
+//only moving to right or left working
+void moveArray(int arr[2][10], int howMuchMove, char whichWay)
+{
+    if(whichWay == 'r')
+    {
+        for(int i =0; i < 2; i++)
+        {
+            for(int k = 0; k < howMuchMove; k++)
+            {
+                int temp = arr[i][10-1];
+                for(int d =10-2; d>=0; d--)
+                {   
+                    arr[i][d+1] = arr[i][d];
+                }
+                arr[i][0] = temp;
+            }
+        }
+    }
+
+    if(whichWay == 'l')
+    {
+        for(int i =0; i < 2; i++)
+        {
+            for(int k = 0; k < howMuchMove; k++)
+            {
+                int temp = arr[i][0];
+                for(int d =0; d<=9; d++)
+                {   
+                    arr[i][d] = arr[i][d+1];
+                }
+                arr[i][9] = temp;
+            }
         }
     }
 }
